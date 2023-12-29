d8b5889ddfebf555d38555d072b5da66b3a8f9fb
Yura Yakovenko <akovenkoura@gmail.com>
2023-12-29 15:09:31 +0200

    remove old cors config

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -34,26 +34,11 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH",
-                "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new
-                UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
-
-    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("**");
+                registry.addMapping("/**").allowedOrigins("/**");
             }
         };
     }
